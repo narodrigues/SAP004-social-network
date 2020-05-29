cfa2ebf841c2614b1a5576579f5c5f3cdba347b7
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-05-29 15:25:14 -0300

    página de registro feita

---
+++ public/pages/register/main.js
@@ -0,0 +1,31 @@
+//import { register } from "./data.js";
+
+export const inscribePage = () => {
+  const inscribePage = document.createElement("section");
+  window.location.href = "#register"
+  inscribePage.innerHTML = `
+    <form class="form-inscribe">
+      <label for="inscribe-name">Nome:
+        <input type="text" class="btn" id="inscribe-name">
+      </label>
+      <label for="inscribe-email">Email: 
+        <input type="email" class="btn" id="inscribe-email">
+      </label>
+      <label for="inscribe-gender">Sexo: 
+        <input type="radio" class="btn" id="inscribe-gender">
+      </label>
+      <label for="age-inscribe">Idade:
+        <input type="date" class="btn" id="age-inscribe">
+      </label>
+      <label for="password-inscribe">Senha:
+        <input type="password" class="btn" id="password-inscribe">
+      </label>
+      <label for="confirm-password-inscribe">Confirme sua senha:
+        <input type="password" class="btn" id="confirm-password-inscribe">
+      </label>
+      <input type="submit" class="btn" id="inscribe-btn" placeholder="Registrar">
+    </form>
+  `;
+
+  return inscribePage;
+}
