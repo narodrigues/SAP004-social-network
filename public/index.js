5c1348898a2cf62a81b9b8437ccced45ef3bed45
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-05-29 15:24:18 -0300

    linkando as SPA's

--- public/index.js
+++ public/index.js
@@ -1,4 +1,30 @@
-// Este é o ponto de entrada de sua aplicação
-import { home } from './pages/home/main.js';
+import { signIn } from './pages/login/main.js';
+import { inscribePage } from './pages/register/main.js';
+import { teste } from './pages/home/main.js';
 
-document.querySelector('#root').appendChild(home());
+const root = document.querySelector('#root');
+
+const init = () => {
+  window.addEventListener("hashchange", () => {
+    root.innerHTML = "";
+    switch(window.location.hash){
+      case "#login":
+        root.appendChild(signIn());
+        break;
+      case "#register":
+        root.appendChild(inscribePage());
+        break;
+      case "#teste":
+        root.appendChild(teste());
+        break;
+      default:
+        root.appendChild(signIn());
+        break;
+    }
+  });
+}
+
+window.addEventListener("load", () => {
+  root.appendChild(signIn());
+  init();
+});
