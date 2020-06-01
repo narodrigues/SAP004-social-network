68481608bae815aec0cc624ad04f815399b3d2a5
natasha <natasha.gr.costa@gmail.com>
2020-06-01 17:29:09 -0300

    ajustes indentação

--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -2,9 +2,9 @@ export const signIn = firebase
   .auth().signInWithEmailAndPassword(email, password)
   .then(() => console.log("login realizado"))
   .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("não foi possível fazer o login pois não existe uma conta")
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("não foi possível fazer o login pois não existe uma conta")
 });
 
 export const signInGoogle = firebase
