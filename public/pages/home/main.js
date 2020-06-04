9bf370a3fb7b10811ab593ae3f7d86d9336c3959
natasha <natasha.gr.costa@gmail.com>
2020-06-04 20:42:16 -0300

    função que publica os posts com nome, data, texto e opções

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -15,48 +15,73 @@ export const feed = () => {
     <img src="">
   </header>
     <main class="main-feed">
-      <section>
+      <section class="user-profile">
         <figure id="user-img"></figure>
         <span id="userName"></span>
       </section>
-      <section class="feed-write-post">
-        <form class="form-feed">
-          <textarea id="post-field" class="post-field" placeholder="O que deseja compartilhar?"></textarea>
-          <div class="post-field-options">
-            <button id="share-post" class="btn">Postar</button>
-            <select id="post-privacy">
-              <option value="private">Privado</option>
-              <option value="public">Público</option>
-            </select>
-          </div>
-        </form>
-      </section>
-      <article id="posts-container"></article>
+      <div>
+        <section class="feed-write-post">
+          <form class="form-feed">
+            <textarea id="post-field" class="post-field" placeholder="O que deseja compartilhar?"></textarea>
+            <div class="post-field-options">
+              <button id="share-post" class="btn">Postar</button>
+              <select id="post-privacy">
+                <option value="private">Privado</option>
+                <option value="public">Público</option>
+              </select>
+            </div>
+          </form>
+        </section>
+        <article class="feed-posts-container" id="posts-container"></article>
+      </div>
     </main>
   `
+
   feedTemplate.querySelector("#share-post").addEventListener("click", () => {
     const postText = document.querySelector("#post-field").value;
     const post = {
-      post: postText
+      post: postText,
+      name: firebase.auth().currentUser.displayName, 
+      timestamps: firebase.firestore.Timestamp.fromDate(new Date()).toDate().toLocaleString('pt-BR')
     }
-    const postsCollection = firebase.firestore().collection("posts").add(post);
-    loadPosts();
+    firebase.firestore().collection("posts").add(post);
+    loadPost();
   });
 
-  const loadPosts = () => {
-    const postsCollection = firebase.firestore().collection("posts");
-    postsCollection
+  const createPosts = (post) => {
+    const postsContainer = feedTemplate.querySelector("#posts-container");
+    const postsOnFeed = document.createElement("section");
+    postsOnFeed.classList.add("div-posts");
+    postsOnFeed.innerHTML = ` 
+      <div class="posted-box-by box">
+        <span class="name-user-published">Publicado por ${post.data().name} em ${post.data().timestamps}</span>
+      </div>
+      <div class="posted-box-text box">
+        <p class="content-post">${post.data().post}</p>
+      </div>
+      <div class="posted-box-options box">
+        <button class="btn-icon"><i class="fas fa-heart icon"></i></button>
+        <button class="btn-icon"><i class="fas fa-comments icon"></i></button>
+      </div>
+    `
+    postsContainer.appendChild(postsOnFeed);  
+  }
+
+  const loadPost = () => {
+    firebase
+    .firestore()
+    .collection("posts")
+    .orderBy('timestamps', 'desc')
+    .limit(5)
     .get()
     .then(database => {
-      database.forEach(doc => {
-        const postsContainer = feedTemplate.querySelector("#posts-container");
-        const postsOnFeed = document.createElement("div");
-        postsOnFeed.classList.add("div-posts");
-        postsContainer.appendChild(postsOnFeed);  
-        postsOnFeed.innerHTML = doc.data().post;
+      database.forEach(post => {
+        createPosts(post)
       });
     });
+    feedTemplate.querySelector("#posts-container").innerHTML = "";
   }
+  loadPost();
 
   return feedTemplate;
 };
