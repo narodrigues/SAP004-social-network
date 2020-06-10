e77cc05d9446016bed75e92b1a58edcbfa538200
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-06-10 00:05:21 -0300

    arrumando função de editar posts

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -1,48 +1,47 @@
-
 export const posts = (text) => {
-	const posts = {
-		post: text,
-		name: firebase.auth().currentUser.displayName,
-		likes: 0,
-		userUid: firebase.auth().currentUser.uid,
-		timestamps: firebase.firestore.Timestamp.fromDate(new Date()).toDate().toLocaleString('pt-BR'),
-	}
-	return firebase
-		.firestore()
-		.collection("posts")
-		.add(posts)
-		.then(() => posts)
+  const posts = {
+    post: text,
+    name: firebase.auth().currentUser.displayName,
+    likes: 0,
+    userUid: firebase.auth().currentUser.uid,
+    timestamps: firebase.firestore.Timestamp.fromDate(new Date()).toDate().toLocaleString('pt-BR'),
+  }
+  return firebase
+    .firestore()
+    .collection("posts")
+    .add(posts)
+    .then(() => posts)
 }
 
 export const loadingPost = () => {
-	return firebase
-		.firestore()
-		.collection("posts")
-		.orderBy('timestamps', 'desc')
-		.limit(5)
-		.get()
-		.then(querySnapshot => {
-			const arrayWithPosts = [];
-			querySnapshot.forEach(doc => {
-				arrayWithPosts.push(doc.data());
-			});
-			return arrayWithPosts;
-		});
+  return firebase
+    .firestore()
+    .collection("posts")
+    .orderBy('timestamps', 'desc')
+    .limit(5)
+    .get()
+    .then(querySnapshot => {
+      const arrayWithPosts = [];
+      querySnapshot.forEach(doc => {
+        arrayWithPosts.push(doc);
+      });
+      return arrayWithPosts;
+    });
 }
 
-export const saveEditPost = (text) => {
-	// const idPost = text.target.dataset.class;
-	return firebase.firestore().collection('posts').doc(docRef.id).update({
-		post: text
-	});  
+export const saveEditPost = (text, id) => {
+  console.log(text, id)
+  return firebase.firestore().collection('posts').doc(id).update({
+    post: text
+  });
 }
 
 export const signOut = () => {
-	firebase
-		.auth()
-		.signOut()
-		.then(() => {
-			window.location.href = "#login";
-		});
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      window.location.href = "#login";
+    });
 }
 
